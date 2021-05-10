f16795e389ca4207e6ffd630b5fddaba4c898694
Lukas <lukas.e.e.ronnberg@gmail.com>
2021-05-10 16:14:00 +0200

    sound

--- js/script.js
+++ js/script.js
@@ -6,3 +6,38 @@
     audio.play();
 
   }
+
+  function hawkeye() {
+    audio.pause();
+    window.audio = new Audio("aux/hawkeye1.mp3");
+    audio.play();
+
+  }
+
+  function hulk() {
+    audio.pause();
+    window.audio = new Audio("aux/hulk1.mp3");
+    audio.play();
+
+  }
+
+  function ironman() {
+    audio.pause();
+    window.audio = new Audio("aux/ironman1.mp3");
+    audio.play();
+
+  }
+
+  function thor() {
+    audio.pause();
+    window.audio = new Audio("aux/thor1.mp3");
+    audio.play();
+
+  }
+
+  function blackwidow() {
+    audio.pause();
+    window.audio = new Audio("aux/blackwidow1.mp3");
+    audio.play();
+
+  }
